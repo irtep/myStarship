10c3c49d8017a6d29633353372abef4e4e6150c4
Glitch (my-starship) <none>
2019-09-12 21:11:17 +0000

    Start of repository

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# myStarship
 Project to build a space adventure game
---
+++ plan.js
@@ -0,0 +1,35 @@
+/*
+STARTSCREEN ------------------------------
+- index.html (start screen)
+  up and center: "upCenter"
+  big greeting
+  
+  left side: "leftSection"
+  buttons: new game, load game
+
+  center side: "centerSection"
+  info about game
+  
+  right side: "rightSection"
+  highscores
+  
+  BUTTONS:
+  new game:
+    clears left and rigth sections
+    left gets a form that asks:
+    name, ships name, job(as a dropdown), avatar.
+    centerSection gets description 
+    rightSection gets image
+    i think it only gives once question at time
+    all come with ok and back-buttons
+    
+- start.js
+  eventlistener for buttons
+  const gameObject
+  
+- start.css
+  black background, with maybe stars
+  white-electric blue texts
+  -------------------------------------
+
+*/
