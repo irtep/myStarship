b92dd0e84140e9a56def33ff8886f7db3351dd8f
Glitch (my-starship) <none>
2019-12-16 08:50:52 +0000

    update 16dec19 morning

--- public/closeCombat/battleFunctions.js
+++ public/closeCombat/battleFunctions.js
@@ -1,19 +1,23 @@
 import { armours, weapons, races, professions } from '../gameData.js';
 import { freezeCopy } from '../helpFunctions.js';
+import { Weapon } from '../classes.js';
+
 // setups character for battle
-export function setupCharacter(character) {
+export function setupCharacter(character, teamNbr) {
   const stats = {
     attacks: null,
     hitPoints: null,
     speed: null,
-    str: null,
-    def: null,
-    bs: null,
-    ws: null
+    str: null, // strength
+    def: null, // defence
+    bs: null, // ballistic skill(shooting)
+    ws: null, // weapon skill(melee)
+    con: null, // constitution
+    save: 6 // armour save, defaults 6, smaller the better
     };
   const myProf = professions.filter(  pro => pro.name === character.profession );
   const myRace = races.filter( rac => rac.name === character.race );
-  const attacks = [{melee: [], ranged: []}];
+  const attacks = {melee: [], ranged: []};
   const myRank = character.rank;
   
   // give stats
@@ -24,21 +28,41 @@ export function setupCharacter(character) {
   stats.def = myProf[0].stats[myRank].def + myRace[0].stats.def;
   stats.bs = myProf[0].stats[myRank].bs;
   stats.ws = myProf[0].stats[myRank].ws;
+  stats.con = myProf[0].stats[myRank].con + myRace[0].stats.con;
+  
+  // get and set armour
   
   // get weapons:
-  character.weapons.forEach( wepa => {
-    console.log('wps', weapons); // error here... continue:
-    const wielded = weapons.filter(wepo => { wepo.name === wepa.name; console.log('compare: ', wepo, wepa); });
-    console.log('wielded: ', freezeCopy(wielded));
-    //console.log('comparing ', wepo, wepa);
-    if (wielded[0].isMeleeWeapon) {
-      attacks.melee.push(wielded[0]);  
-      // adjust power
-      attacks.melee[attacks.melee.length-1].power += stats.str;
-    } else {
-      attacks.ranged.push(wielded[0]);
-    }
-  });
+  if (character.weapons.length > 0) {
+    
+    character.weapons.forEach( wepa => {
+      const wielded = weapons.filter( wepo => wepo.name === wepa);
+      
+      // make attacks
+      if (wielded[0].isMeleeWeapon) {
+        attacks.melee.push(wielded[0]);  
+        // adjust power
+        attacks.melee[attacks.melee.length-1].power += stats.str;
+      } else {
+        attacks.ranged.push(wielded[0]);
+      }
+    });
+  } else {
+    
+    // if character doesn't have any weapons
+    // create unarmed attack:
+    const unarmedAttack = new Weapon(myRace[0].unarmed, 0, 0, null, stats.str, 0, 0, 20, true, null, null);
+    
+    attacks.melee.push(unarmedAttack);
+  }
+  
+  // then special attacks like magic and special technics
+  /* not yet made */
+  
+  // add stats, attacks and team number to character
+  character.stats = stats;
+  character.attacks = attacks;
+  character.teamNbr = teamNbr;
   
   return character;
 }
